08c0aa64a9f89a9915bad992bc1d93d9fe3f8575
yxw <AaaNeei@136.com>
2019-04-27 16:47:22 +0800

    游学网全局配置中心

---
+++ yxw_config/src/main/java/com/yxw/yxw_config/YxwConfigApplication.java
@@ -0,0 +1,13 @@
+package com.yxw.yxw_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxwConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxwConfigApplication.class, args);
+    }
+
+}
