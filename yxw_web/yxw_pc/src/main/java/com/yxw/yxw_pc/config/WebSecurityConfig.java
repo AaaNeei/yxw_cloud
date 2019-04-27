b7d1cb94d4e2f08e725cdfbd30d329fe69b59aa7
yxw <AaaNeei@136.com>
2019-04-27 16:36:19 +0800

    eureka安全登录释放pc工程的csrf

---
+++ yxw_web/yxw_pc/src/main/java/com/yxw/yxw_pc/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.yxw.yxw_pc.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * eureka安全登录问题,
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+        // Configure HttpSecurity as needed (e.g. enable http basic).
+        /*http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        // 如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();*/
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //配置需要忽略验证的地址
+        web.ignoring().antMatchers("/eureka/**");
+        web.ignoring().antMatchers("/**");
+    }
+
+}
