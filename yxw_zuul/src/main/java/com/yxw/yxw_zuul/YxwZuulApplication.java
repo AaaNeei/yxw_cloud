4e3271f01efded92fcc99857e4c92d901ee6e261
yxw <AaaNeei@136.com>
2019-04-27 16:22:09 +0800

    游学网网关zuul

---
+++ yxw_zuul/src/main/java/com/yxw/yxw_zuul/YxwZuulApplication.java
@@ -0,0 +1,13 @@
+package com.yxw.yxw_zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxwZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxwZuulApplication.class, args);
+    }
+
+}
